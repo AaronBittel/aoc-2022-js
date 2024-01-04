f81fe2b82ee8c5371f3a81f284d28c013cb22f70
Aaron Bittel <aaron@bittels.de>
2024-01-04 19:25:53 +0100

    Day 6 now outputs solutions for puzzle 1 and 2

--- day6/solution.js
+++ day6/solution.js
@@ -7,35 +7,43 @@ function load(file) {
 	return input = fs
 		.readFileSync(path.join(__dirname, file), 'utf8')
 		.toString()
-		.split("\r\n")
+		.split("\n")
 }
 
 
-function solve(p) {
-    const round = p.map(row => {
-        const arr = Array.from(row)
-        let four = arr.slice(0, 14)
-        if (new Set(four).size === 14) {
-            return 14
+function find_first_unique_values(str, count) {
+    arr = Array.from(str)
+    let arrSlice = arr.slice(0, count)
+    if (new Set(arrSlice).size === count) {
+        return count
+    }
+    for (let i = count; i < arr.length; ++i) {
+        arrSlice.shift()
+        arrSlice.push(arr[i])
+        if (new Set(arrSlice).size === count) {
+            return i + 1
         }
-        for (let i = 14; i < row.length; ++i) {
-            four.shift()
-            four.push(row[i])
-            if (new Set(four).size === 14) {
-                return i + 1
-            }
-        }
-    })
-    return round
+    }
 }
 
+
+function solve(p, count) {
+    if (p.length > 1) {
+        return solution = p.map(row => find_first_unique_values(row, count))
+    } else {
+        return solution = find_first_unique_values(p[0], count);
+    }
+}
 
 
 function main() {
 	const startTime = performance.now()
-	const solPart1 = solve(load("./input_.txt"))
+    const puzzle = load("./input.txt")
+	const solPart1 = solve(puzzle, 4)
+    const solPart2 = solve(puzzle, 14)
 
 	console.log("Solution Part 1:", solPart1)
+    console.log("Solution Part 2:", solPart2)
 	console.log("Solved in " + ((performance.now() - startTime) / 1000).toFixed(5) + " Sec.")
 }
 
